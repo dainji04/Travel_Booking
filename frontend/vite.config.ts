f5dbea8989b456064e398d896f90629447edd40e
dainji04 <ndai31072004@gmail.com>
2025-04-21 21:52:12 +0700

    fix base url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/test-deloy-travel',
+    // base: '/test-deloy-travel',
     plugins: [vue(), vueDevTools()],
     resolve: {
         alias: {
