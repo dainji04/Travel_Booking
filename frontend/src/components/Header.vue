0be444072cf082bbf0c139c4fd1b26ac3a9fe052
dainji04 <ndai31072004@gmail.com>
2025-05-08 20:10:58 +0700

    step 2 checkout: complete

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,81 +1,105 @@
 <script setup lang="ts">
-import {authStore} from '@/stores/auth';
-import {computed, onMounted, ref, useTemplateRef} from 'vue';
+import { authStore } from '@/stores/auth';
+import { computed, onMounted, ref, useTemplateRef } from 'vue';
 
 const useAuth = authStore();
+const isShowTour = ref(false);
 
 const checkLogin = computed(() => useAuth.isAuthenticated);
-const user = computed(() => useAuth.user);
+// const user = computed(() => useAuth.user);
 
-const ScrollToTop = ref(false);
+// const ScrollToTop = ref(false);
 
-const toTop = () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  });
-};
+// const toTop = () => {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: 'smooth',
+//   });
+// };
 
-onMounted(() => {
-  window.addEventListener('scroll', () => {
-    let a = 0;
-    a = window.scrollY;
+// onMounted(() => {
+//   window.addEventListener('scroll', () => {
+//     let a = 0;
+//     a = window.scrollY;
 
-    if (a > 50) {
-      a = 50;
-    }
+//     if (a > 50) {
+//       a = 50;
+//     }
 
-    if (window.scrollY > 100) {
-      ScrollToTop.value = true;
-    } else {
-      ScrollToTop.value = false;
-    }
-  });
-});
+//     if (window.scrollY > 100) {
+//       ScrollToTop.value = true;
+//     } else {
+//       ScrollToTop.value = false;
+//     }
+//   });
+// });
 </script>
 
 <template>
-  <header class="header">
-    <div class="navbar">
-      <ul class="list">
-        <router-link :to="{ name: 'home' }">
-          <li class="list__link">Home</li>
-        </router-link>
-        <router-link :to="{ name: 'tour' }">
-          <li class="list__link">Tour</li>
-        </router-link>
-        <router-link :to="{ name: 'about' }">
-          <li class="list__link">About Us</li>
-        </router-link>
-        <router-link :to="{ name: 'blog' }">
-          <li class="list__link">Blog</li>
-        </router-link>
-        <router-link :to="{ name: 'contact' }">
-          <li class="list__link">Contact</li>
-        </router-link>
-      </ul>
-      <div class="options">
-        <router-link v-if="checkLogin" class="options__avatar" :to="{ name: 'profile' }">
-          <div class="avatar_user"></div>
-        </router-link>
-        <router-link v-else class="list__link" :to="{ name: 'login' }">
-          Login
-        </router-link>
-      </div>
-    </div>
-    <div class="logo">
-      <div class="logo__box">
-        <img
-            loading="lazy"
-            class="logo__image"
-            src="@/assets/fonts/logo.svg"
-            alt=""
-        />
-      </div>
-    </div>
-  </header>
-  <!--    <button class="scroll-to-top" v-if="ScrollToTop" @click="toTop">top</button>-->
-  <a-back-top/>
+    <header class="header">
+        <div class="navbar">
+            <div class="logo">
+                <img src="@/assets/images/admin/logo.png" alt="logo" />
+            </div>
+            <ul class="list">
+                <router-link class="list-item" :to="{ name: 'home' }">
+                    <li class="list__link">Home</li>
+                </router-link>
+                <div
+                    @mousemove="isShowTour = true"
+                    @mouseleave="isShowTour = false"
+                    class="tour list-item"
+                >
+                    <li class="list__link">Tour</li>
+                    <ul class="list_link-option" v-show="isShowTour">
+                        <router-link :to="{ name: 'tour' }">
+                            <li class="list_link-option-item">
+                                tour available
+                            </li>
+                        </router-link>
+                        <router-link :to="{ name: 'tourOrder' }">
+                            <li class="list_link-option-item">
+                                create your tour
+                            </li>
+                        </router-link>
+                    </ul>
+                </div>
+                <router-link class="list-item" :to="{ name: 'about' }">
+                    <li class="list__link">About Us</li>
+                </router-link>
+                <router-link class="list-item" :to="{ name: 'blog' }">
+                    <li class="list__link">Blog</li>
+                </router-link>
+                <router-link class="list-item" :to="{ name: 'contact' }">
+                    <li class="list__link">Contact</li>
+                </router-link>
+            </ul>
+            <div class="options">
+                <router-link
+                    v-if="checkLogin"
+                    class="options__avatar"
+                    :to="{ name: 'profile' }"
+                >
+                    <div class="avatar_user"></div>
+                </router-link>
+                <router-link v-else class="list__link" :to="{ name: 'login' }">
+                    Login
+                </router-link>
+            </div>
+        </div>
+        <!-- <div class="logo">
+            <div class="logo__box">
+                <img
+                    loading="lazy"
+                    class="logo__image"
+                    src="@/assets/fonts/logo.svg"
+                    alt=""
+                />
+            </div>
+        </div> -->
+    </header>
+    <!--    <button class="scroll-to-top" v-if="ScrollToTop" @click="toTop">top</button>-->
+    <a-back-top />
 </template>
 
 <style lang="scss" scoped>
--- frontend/src/views/Checkout.vue
+++ frontend/src/views/Checkout.vue
@@ -1,39 +1,30 @@
 <script setup lang="ts">
-import Pagination from "@/components/Pagination.vue";
+import { useRoute } from 'vue-router';
+import Pagination from '@/components/Pagination.vue';
+
+import ProgressBarCheckOut from '@/components/ProgressBarCheckOut.vue';
+
+const route = useRoute();
+
+// Access query parameters
+const date = route.query.date;
+const adults = route.query.adults;
+const children = route.query.children;
+const tourId = route.query.tourId;
+
+console.log(date, adults, children, tourId); // Debugging: Check the values
 </script>
 
 <template>
-  <pagination title="Check out" :link="['home', 'checkout']" :pagination="['Homepage', 'Check out']"/>
-  <div class="checkout">
-    <div class="progress">
-      <div class="step1 progress_step">
-        <img v-if="false" src="@/assets/fonts/check-circle.svg" alt="check-circle">
-        <div v-else class="progress_number">1</div>
-        <p>Select Date</p>
-      </div>
-      <hr class="line active"/>
-      <div class="step2 progress_step">
-        <img v-if="false" src="@/assets/fonts/check-circle.svg" alt="check-circle">
-        <div v-else class="progress_number">2</div>
-        <p>Tourist</p>
-      </div>
-      <hr class="line"/>
-      <div class="step3 progress_step">
-        <img v-if="false" src="@/assets/fonts/check-circle.svg" alt="check-circle">
-        <div v-else class="progress_number">3</div>
-        <p>Payment Details</p>
-      </div>
-      <hr class="line"/>
-      <div class="step4 progress_step">
-        <img v-if="false" src="@/assets/fonts/check-circle.svg" alt="check-circle">
-        <div v-else class="progress_number">4</div>
-        <p>Payment</p>
-      </div>
+    <pagination
+        title="Check out"
+        :link="['home', 'checkout']"
+        :pagination="['Homepage', 'Check out']"
+    />
+    <div class="checkout">
+        <ProgressBarCheckOut :step="2" />
+        <div class="container"></div>
     </div>
-    <div class="container">
-
-    </div>
-  </div>
 </template>
 
 <style scoped lang="scss">
