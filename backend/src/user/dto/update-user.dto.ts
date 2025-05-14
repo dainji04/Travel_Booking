bee8acca32632ef8cf999f891aca2f5b3b8d502c
thinh <nguyenphuthinh040507@gmail.com>
2025-05-14 20:20:22 +0700

    Docs updateUser

--- backend/src/user/dto/update-user.dto.ts
+++ backend/src/user/dto/update-user.dto.ts
@@ -1,4 +1,31 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Roles } from 'src/util/common/role_User.common';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto  {
+
+     @ApiProperty({example:'Dai Den', description:'Username'})
+        @IsNotEmpty()
+        @IsString()
+        name:string
+    
+        @ApiProperty({example:'daiden3101@gmail.com',description:'User email'})
+        @IsNotEmpty()
+        @IsEmail()
+        email:string
+    
+        @ApiProperty({example:'daidenPassword',description:'User password'})
+        @IsNotEmpty()
+        password:string
+    
+        @ApiProperty({
+            example: [Roles.USER],
+            description: 'User roles',
+            enum: Roles,
+            isArray: true,
+        })
+        @IsNotEmpty()
+        roles:Roles[]
+}
